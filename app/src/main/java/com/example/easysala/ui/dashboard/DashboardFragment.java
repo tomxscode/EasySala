8c9efa7b4ba14049486ef6f602dc5837a3a77087
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-03 08:52:54 -0300

    Removiendo cosas inncesarias del menú

--- app/src/main/java/com/example/easysala/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/easysala/ui/dashboard/DashboardFragment.java
@@ -18,14 +18,9 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
