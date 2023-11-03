574bb7e38cb86acf88dc7fe2ca488471dc25ea84
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-03 10:19:02 -0300

    Fragmento login

--- app/src/main/java/com/example/easysala/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/easysala/ui/home/HomeFragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
+import com.example.easysala.R;
 
 import com.example.easysala.databinding.FragmentHomeBinding;
+import com.example.easysala.ui.login.LoginFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -21,6 +25,7 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         return root;
     }
 
