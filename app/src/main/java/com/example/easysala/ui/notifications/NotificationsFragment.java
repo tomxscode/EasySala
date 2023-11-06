84e7250327a06fce8a6938bda523e748cc586dfc
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-06 09:21:28 -0300

    Cambiar de fragmentos
    Permite cambiar fragmentos al hacer click sobre un botón, tomó 1 semana hacerlo.

--- app/src/main/java/com/example/easysala/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/easysala/ui/notifications/NotificationsFragment.java
@@ -4,24 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.easysala.R;
 import com.example.easysala.databinding.FragmentNotificationsBinding;
+import com.example.easysala.ui.login.LoginFragment;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button btn2 = (Button) root.findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cargarFragmento(new LoginFragment());
+            }
+        });
+
         return root;
+    }
+
+
+    public void cargarFragmento(Fragment fragmento) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.remove(new NotificationsFragment());
+        fragmentTransaction.replace(R.id.frameLayout, fragmento);
+        fragmentTransaction.commit();
+    }
+
+    public void botonAccion(View v) {
+        cargarFragmento(new LoginFragment());
     }
 
     @Override
