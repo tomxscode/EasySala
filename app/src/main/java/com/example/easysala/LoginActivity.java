18b1db6e40449fba4612faad3636270e1dd6d63c
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-08 09:09:35 -0300

    Agregado google services

---
+++ app/src/main/java/com/example/easysala/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.easysala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Integración de firebase Auth
+    }
+}
