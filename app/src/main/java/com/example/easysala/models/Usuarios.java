a6c4f18c345321a3ee4c1553b02b24203cfb7c38
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-10 09:00:06 -0300

    Implementación de firebase

--- app/src/main/java/com/example/easysala/models/Usuarios.java
+++ app/src/main/java/com/example/easysala/models/Usuarios.java
@@ -1,5 +1,17 @@
 package com.example.easysala.models;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Usuarios {
 
     private String documentId;
@@ -8,6 +20,7 @@ public class Usuarios {
     private String correo;
     private int rol;
     private boolean sesionIniciada;
+    private String uid_bd;
 
     public Usuarios(String documentId, String nombre, String apellido, String correo, int rol, boolean sesionIniciada) {
         this.documentId = documentId;
@@ -21,6 +34,40 @@ public class Usuarios {
     public Usuarios(String documentId, String correo) {
         this.documentId = documentId;
         this.correo = correo;
+    }
+
+    public Usuarios(String nombre, String apellido, String correo, int rol, String documentId, String uid_bd) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.rol = rol;
+        this.documentId = documentId;
+        this.uid_bd  = uid_bd;
+    }
+
+    public void guardarBd() {
+        FirebaseFirestore  db = FirebaseFirestore.getInstance();
+        Map<String, Object> usuario = new HashMap<>();
+        usuario.put("nombre", nombre);
+        usuario.put("apellido", apellido);
+        usuario.put("correo", correo);
+        usuario.put("rol", rol);
+        usuario.put("uid_user", documentId);
+
+        db.collection("usuario")
+                .add(usuario)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        uid_bd = documentReference.getId();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("error");
+                    }
+                });
     }
 
     public String getDocumentId() {
