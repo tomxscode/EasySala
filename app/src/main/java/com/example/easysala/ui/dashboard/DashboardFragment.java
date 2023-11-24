19d289cf89eb362c633c9aa06a6394ec9991b415
NaxBv <149114537+NaxBv@users.noreply.github.com>
2023-11-23 23:55:19 -0300

    Listar reservas en dashboard
    Falta ver el detalle de que se solapen los fragmentos

--- app/src/main/java/com/example/easysala/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/easysala/ui/dashboard/DashboardFragment.java
@@ -7,11 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayoutStates;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -19,12 +23,15 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.easysala.AprobarReservas;
 import com.example.easysala.ListAdapter;
 import com.example.easysala.ListElement;
+import com.example.easysala.MainActivity;
 import com.example.easysala.R;
 import com.example.easysala.databinding.FragmentDashboardBinding;
 import com.example.easysala.models.CallbackHorario;
 import com.example.easysala.models.Horario;
+import com.example.easysala.ui.implementos.listar_implementos;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -99,12 +106,38 @@ public class DashboardFragment extends Fragment {
             }
 
         });
+        Button btn_aprobar = view.findViewById(R.id.btn_aprobar); // Reemplaza con el ID real de tu botón
+
+        // Verifica el rol del usuario y establece la visibilidad del botón
+        if (MainActivity.rolUsuario > 2) {
+            btn_aprobar.setVisibility(View.VISIBLE);
+            Log.d("MainActivity", "si aparece :) rol =" + MainActivity.rolUsuario);
+        } else {
+            btn_aprobar.setVisibility(View.GONE);
+            Log.d("MainActivity", "no aparece :( rol = " + MainActivity.rolUsuario);
+        }
+        btn_aprobar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarFragmento(new AprobarReservas());
+            }
+        });
         return view;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+    private void cargarFragmento(Fragment fragment) {
+
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null); // Opcional, para agregar a la pila de retroceso
+        fragmentTransaction.commit();
+    }
+
 }
